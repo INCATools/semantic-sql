ee32e24107b60d154b6ec97fb53c0d4ec17bb96b
cmungall <cjm@berkeleybop.org>
2022-02-16 18:06:51 -0500

    fixed test

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -15,18 +15,19 @@ DB_DIR = os.path.join(cwd, 'inputs')
 OUTPUT_DIR = os.path.join(cwd, 'outputs')
 
 class CRUDTestCase(unittest.TestCase):
-    path = os.path.join(DB_DIR, 'go-nucleus.db')
-    edit_path = os.path.join(OUTPUT_DIR, 'edit.db')
-    subprocess.run(['cp', path, edit_path])
-    engine = create_engine(f"sqlite:///{edit_path}")
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    q = session.query(Statements).filter(Statements.predicate == 'rdfs:label').filter(Statements.value.like('% activity'))
-    for t in q.all():
-        print(f'{t.subject} {t.value}')
-        # https://github.com/ontodev/rdftab.rs/issues/16
-        ## t.value = t.value.replace(' activity', '')
-    #session.commit()
+    def test_crud(self):
+        path = os.path.join(DB_DIR, 'go-nucleus.db')
+        edit_path = os.path.join(OUTPUT_DIR, 'edit.db')
+        subprocess.run(['cp', path, edit_path])
+        engine = create_engine(f"sqlite:///{edit_path}")
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        q = session.query(Statements).filter(Statements.predicate == 'rdfs:label').filter(Statements.value.like('% activity'))
+        for t in q.all():
+            print(f'{t.subject} {t.value}')
+            # https://github.com/ontodev/rdftab.rs/issues/16
+            ## t.value = t.value.replace(' activity', '')
+        #session.commit()
 
 
 
